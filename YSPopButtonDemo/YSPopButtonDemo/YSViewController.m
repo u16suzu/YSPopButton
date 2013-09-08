ee2d4d3fd5686a9b8d8a6c3a065ba9a01dfb2112
u16suzu <u16suzu@gmail.com>
2013-09-09 00:13:32 +0900

    デモプロジェクト自体に 画像ファイルを追加.

--- YSPopButtonDemo/YSPopButtonDemo/YSViewController.m
+++ YSPopButtonDemo/YSPopButtonDemo/YSViewController.m
@@ -19,9 +19,22 @@
 {
     [super viewDidLoad];
     
-
-    YSPopButton *btn = [[YSPopButton alloc]initWithFrame:CGRectMake(0, 0, 60, 60)];
-    [self.view addSubview:btn];
+    
+    // PopButton
+    YSPopButton *pbtn = [[YSPopButton alloc]initWithFrame:CGRectMake(0, 0, 60, 60)];
+    pbtn.center = CGPointMake(self.view.center.x, self.view.center.y - 100);
+    UIImage *pbtn_img = [UIImage imageNamed:@"arrow_left"];
+    [pbtn setImage:pbtn_img forState:UIControlStateNormal];
+    pbtn.adjustsImageWhenHighlighted = NO;
+    [self.view addSubview:pbtn];
+    
+    // FlatPopButton
+    YSPopButton *flat_pbtn = [[YSPopButton alloc]initWithFrame:CGRectMake(0, 60, 60, 60)];
+    flat_pbtn.center = self.view.center;
+    UIImage *flat_pbtn_img = [UIImage imageNamed:@"arrow_left"];
+    [flat_pbtn setFlatImage:flat_pbtn_img :[UIColor darkGrayColor]];
+    flat_pbtn.adjustsImageWhenHighlighted = NO;
+    [self.view addSubview:flat_pbtn];
     
 }
 
